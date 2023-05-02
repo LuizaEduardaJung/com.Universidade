b77ad3592e11021e57bbb9b981c5cead4a762606
LuizaEduardaJung <luiza-ejung@educar.rs.gov.br>
2023-05-02 20:01:28 -0300

    criando classe disciplina

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -9,5 +9,47 @@
  * @author Escola
  */
 public class Disciplina {
-    
+   private int codigo;
+   private String nome;
+   private int numeroDeVagas;
+
+    public Disciplina(int codigo, String nome, int numeroDeVagas) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.numeroDeVagas = numeroDeVagas;
+    }
+
+    public Disciplina() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroDeVagas() {
+        return numeroDeVagas;
+    }
+
+    public void setNumeroDeVagas(int numeroDeVagas) {
+        this.numeroDeVagas = numeroDeVagas;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina{" + "codigo=" + codigo + ", nome=" + nome + ", numeroDeVagas=" + numeroDeVagas + '}';
+    }
+   
+   
 }
